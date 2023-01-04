db948f5e7deff3067e488b4a1d1def342212775c
angelina <angelinaneunzig@gmx.de>
2023-01-04 10:41:11 +0100

    javascript exercises

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("hello from script")
+const age = 40 //defining number variable
+console.log(age)
+const multipl = 1.2
+console.log(age * multipl) //simple maths
+const name = "angelina"
+console.log("welcome " + name) //concatination of strings
+// alert(name)
+const userage = prompt("how old are you") //build in javascript function
+console.log(userage)
+if (userage > 17) { //conditional 
+    alert("welcometothewebsite")
+} else if (userage === 40) {
+    alert("hey, you have the same age as me")
+} else {
+
+    alert("yourtooyoung")
+}
